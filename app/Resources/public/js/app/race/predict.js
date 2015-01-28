78a7eb25c6fd59f93492876aa2bf6fdf88f290d1
Brieuc Thomas <brieuc.thomas@orange.com>
2015-01-28 14:02:08 +0100

    Working on prediction page

--- app/Resources/public/js/app/race/predict.js
+++ app/Resources/public/js/app/race/predict.js
@@ -1,32 +1,60 @@
 require(['jquery', 'sortable'], function ($, Sortable) {
 
-    var form = $('[name=prediction]');
-    var list = $('#list-teams');
-    var limit = form.find('select').length;
+    var form = $('[name=prediction]'),
+        list = $('#list-teams');
 
-    form
-        .find('.form-group:first')
-            .addClass('no-sr-only')
-            .end()
-        .find('option:selected').each(function() {
-            list.append($('<li>').data('id', this.value).append(this.text));
-        });
+    form.find('.form-group:first').addClass('no-sr-only');
 
-    list.sortable().bind('sortupdate', function(e, ui) {
-        form.find('select').each(function(index, el){
-            var id = list.find('> li').eq(index).data('id');
-            $(this).val(id);
-        });
-
-        updateList(list, limit);
+    form.find('select').on('change', function () {
+        updateListFromForm(list, form);
     });
 
-    updateList(list, limit);
+    list.sortable({
+        onDrop: function (item, targetContainer, _super) {
+            updateFormFromList(form, list);
+        }
+    });
+
+    updateListFromForm(list, form);
 });
 
+function removeListItems(list) {
+    list.find('> li').remove();
+}
 
-function updateList(list, limit)
-{
+function updateListFromForm(list, form) {
+    var limit = form.find('select').length;
+
+    removeListItems();
+
+    form.find('select').first().find('> option').each(function () {
+        appendListItemFromSelect(list, $(this));
+    });
+
+    form.find('options:selected').each(function () {
+        removeListItemFromSelect(list, $(this));
+        prependListItemFromSelect(list, $(this));
+    });
+
     list.find('> li').removeClass('bg-success').slice(0, limit).addClass('bg-success');
 }
 
+
+function updateFormFromList(form, list) {
+    form.find('select').each(function (index, el) {
+        var id = list.find('> li').eq(index).data('id');
+        $(this).val(id);
+    });
+}
+
+function prependListItemFromSelect(list, select) {
+    list.prepend($('<li>').data('id', select.val()).append(select.text()));
+}
+
+function appendListItemFromSelect(list, select) {
+    list.append($('<li>').data('id', select.val()).append(select.text()));
+}
+
+function removeListItemFromSelect(list, select) {
+    list.find('li[data-id=' + select.val() + ']').remove();
+}
