488127d39e6d264f552aa8db5382a262195b0468
Brieuc Thomas <tbrieuc@gmail.com>
2015-02-11 13:29:41 +0100

    CS

--- app/Resources/assets/js/app/homepage.js
+++ app/Resources/assets/js/app/homepage.js
@@ -1,11 +1,10 @@
 require(['jquery', 'countdown'], function ($, cd) {
     var node = $('#next-race-remaining-time');
     if (node.length) {
-        var format = node.html();
         node
             .countdown(node.data('date'))
             .on('update.countdown', function (event) {
-                $(this).html(event.strftime(format));
+                $(this).html(event.strftime(node.data('format')));
             });
     }
 });
