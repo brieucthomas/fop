e91d3838146fe9f8713a44d40e4549872c520b6d
Brieuc Thomas <tbrieuc@gmail.com>
2015-02-16 13:37:06 +0100

    Adding a table prefix

---
+++ src/AppBundle/Listener/TablePrefixListener.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * (c) Brieuc Thomas <tbrieuc@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+namespace AppBundle\Listener;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+
+/**
+ * @author Brieuc Thomas <tbrieuc@gmail.com>
+ */
+class TablePrefixListener
+{
+    protected $prefix;
+
+    public function __construct($prefix)
+    {
+        $this->prefix = (string) $prefix;
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+        $classMetadata->setTableName($this->prefix . $classMetadata->getTableName());
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+                $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+}
