82dabf2ba60f44f762ba9eb9f793ee34252f205e
Brieuc Thomas <tbrieuc@gmail.com>
2015-02-16 12:29:53 +0100

    Fixing config

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,22 +1,11 @@
 <?php
 
-if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
-    $url = parse_url($_ENV['CLEARDB_DATABASE_URL']);
-
+if (isset($_ENV['DATABASE_URL']) && (false !== $url = parse_url($_ENV['DATABASE_URL']))) {
     $container->setParameter('database_host', $url['host']);
     $container->setParameter('database_port', $url['port']);
     $container->setParameter('database_name', trim($url['path'], '/'));
     $container->setParameter('database_user', $url['user']);
     $container->setParameter('database_password', $url['pass']);
-}
-
-if (isset($_ENV['POSTMARK_SMTP_SERVER'])) {
-    $container->setParameter('mailer_transport', 'swift_transport.postmark');
-    $container->setParameter('mailer_host', $_ENV['POSTMARK_SMTP_SERVER']);
-}
-
-if (isset($_ENV['POSTMARK_API_KEY'])) {
-    $container->setParameter('mailer_token', $_ENV['POSTMARK_API_KEY']);
 }
 
 if (isset($_ENV['SYMFONY_SECRET'])) {
