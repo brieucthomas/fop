16ddc2420ac5dd945ed4eb347d7dd6cb33b6758d
Brieuc Thomas <tbrieuc@gmail.com>
2015-02-23 10:02:33 +0100

    Adding mailer options

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -16,6 +16,14 @@ if (isset($_ENV['MAILER_PORT'])) {
     $container->setParameter('mailer_port', $_ENV['MAILER_PORT']);
 }
 
+if (isset($_ENV['MAILER_ENCRYPTION'])) {
+    $container->setParameter('mailer_encryption', $_ENV['MAILER_ENCRYPTION']);
+}
+
+if (isset($_ENV['MAILER_AUTH_MODE'])) {
+    $container->setParameter('mailer_auth_mode', $_ENV['MAILER_AUTH_MODE']);
+}
+
 if (isset($_ENV['MAILER_USER'])) {
     $container->setParameter('mailer_user', $_ENV['MAILER_USER']);
 }
@@ -27,4 +35,3 @@ if (isset($_ENV['MAILER_PASSWORD'])) {
 if (isset($_ENV['SYMFONY_SECRET'])) {
     $container->setParameter('secret', $_ENV['SYMFONY_SECRET']);
 }
-
