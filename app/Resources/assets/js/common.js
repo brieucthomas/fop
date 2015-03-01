297211b19769ca37794047592479bc795144c725
Brieuc Thomas <tbrieuc@gmail.com>
2015-03-01 13:30:44 +0100

    Localize times

--- app/Resources/assets/js/common.js
+++ app/Resources/assets/js/common.js
@@ -1,3 +1,7 @@
-require(['jquery', 'bootstrap'], function ($, twbs) {
+require(['jquery', 'bootstrap', 'moment'], function ($, twbs, moment) {
     $('[data-toggle="tooltip"], [data-hover="tooltip"]').tooltip();
+
+    $('[data-load="localize"]').each(function(index, node){
+        $(node).text(moment.utc($(node).data('date')).local().format($(node).data('format')));
+    });
 });
